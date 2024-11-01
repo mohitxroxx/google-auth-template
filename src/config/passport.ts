43d21c5b01cd47b956fb5da5234f112f1456d2e2
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-11-02 01:17:34 +0530

    OAuth2 Google done!

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -6,7 +6,7 @@ import UserSchema from '../models/model'
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID || '',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    callbackURL: 'http://localhost:5000/auth/google/callback'
+    callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
